8faab2a375ff42817390ed6cc5874c78edf8cc19
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-07-05 18:47:07 +0200

    added cart functionality for one product

--- src/App.js
+++ src/App.js
@@ -5,33 +5,94 @@ import './App.css'
 
 
 function App() {
-
+  // We set the initial state of the cart to an empty object
   const [productData, setData] = useState({
-    title: "Your chart is empty",
+    title: "Your cart is empty",
     price: 0
   });
 
-
+//Obtain the product data from the Products component and pass it to the Cart component
   const showCart = (product) => {
     setData({
       ...productData,
       title: product.title,
-      price: product.price
-    })
+      price: product.price,
+      img: product.img
+    });
 
+    setTotalPrice(product.price);
   }
+
+
+
+  // Function to manage the cart, adding or removing products from the cart, also calculating the total price
+  const [totalPrice, setTotalPrice] = useState(productData.price);
+  const [numberOfProducts, setNumberOfProducts] = useState(1);
+  const addProduct = () => {
+    setNumberOfProducts(numberOfProducts + 1);
+    setTotalPrice(totalPrice + productData.price);
+  }
+
+  const removeProduct = () => {
+    setNumberOfProducts(numberOfProducts - 1);
+    setTotalPrice(totalPrice - productData.price);
+
+    // If the number of products is 0, the user can't remove more number of products
+    if (numberOfProducts === 0) {
+      setNumberOfProducts(0);
+    }
+
+    // If the total price is 0, the user can't remove more number of products
+    if (totalPrice === 0) {
+      setTotalPrice(0);
+    }
+  }
+
+  //If the user clicks on the "Buy now" button, the product data is passed to the Cart component, and the Cart component is displayed
+  const checkCart = () => {
+    if (productData.title === "Your cart is empty") {
+      return (
+        <>
+        <div className="cart">
+            <h1>Shopping Cart</h1>
+            <hr></hr>
+            <div className="cart__product">
+                  <div className="cart__product--info">
+                    <h2>{productData.title}</h2>
+                  </div>
+            </div>
+            <h2>Total: 0€</h2>
+            <hr></hr>
+            <button>Checkout</button>
+        </div>
+      </>
+      );
+    } else {
+      return (
+        <Cart
+          title={productData.title}
+          price={productData.price}
+          img={productData.img}
+          addProduct={addProduct}
+          substractProduct={removeProduct}
+          numberOfProducts={numberOfProducts}
+          totalPrice={totalPrice}
+        />
+      );
+    }
+  }
+
 
   return (
     <>
     <div className="App">
         <Products manageClick = {showCart} />
-        <Cart 
-          title = {productData.title}
-          price = {productData.price + "€"} 
-        />
+        {checkCart()}
     </div>
     </>
   )
+
+  
 }
 
 export default App;
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,26 +1,25 @@
 import React from 'react'
-import '../styles/Chart/chart.css'
-import img1 from '../img/img_1.jpeg'
+import '../styles/Cart/cart.css'
 
-const Chart = ({title, price, img}) => {
+const Cart = ({title, price, img, addProduct, substractProduct, numberOfProducts, totalPrice}) => {
   return (
     <>
       <div className="cart">
           <h1>Shopping Cart</h1>
           <hr></hr>
           <div className="cart__product">
-            <img src={img1} alt="" />
+            <img src={img} alt="" />
                 <div className="cart__product--info">
                   <h3>{title}</h3>
-                  <h3>{price}</h3>
-                  <button >-</button>
-                  <button >1</button>
-                  <button >+</button>
+                  <h3>{price}€</h3>
+                  <button onClick={substractProduct}>-</button>
+                  <button >{numberOfProducts}</button>
+                  <button onClick={addProduct}>+</button>
                   <button className='removeBtn'>Remove</button>
                 </div>
           </div>
           <hr></hr>
-          <h2>Total: 0€</h2>
+          <h2>Total: {totalPrice}€</h2>
           <hr></hr>
           <button>Checkout</button>
       </div>
@@ -30,4 +29,4 @@ const Chart = ({title, price, img}) => {
 }
 
 
-export default Chart
+export default Cart
