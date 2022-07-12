030e9d546be5557a4ce56a0084fb08aeec115137
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-07-12 13:00:26 +0200

    deleted unused code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
-import LoginPage from './components/Login/LoginPage'
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 
@@ -11,7 +10,6 @@ root.render(
   // <React.StrictMode>
   <BrowserRouter>
     <App />
-    {/* <LoginPage /> */}
   </BrowserRouter>
   // </React.StrictMode>
 );
