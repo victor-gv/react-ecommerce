1bbc514ccd53f152f59170088e3577db208beb00
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-07-17 18:18:43 +0200

    second form added on login page

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -1,24 +1,23 @@
-import React from 'react'
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
+import React from "react";
+import Avatar from "@mui/material/Avatar";
+import Button from "@mui/material/Button";
+import CssBaseline from "@mui/material/CssBaseline";
+import TextField from "@mui/material/TextField";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useEffect, useState } from "react";
 import NavbarLogin from "../NavbarLogin/NavbarLogin";
 import Cart from "../Cart/Cart";
 import ProductItem from "../ProducItem/ProductItem";
-import emptyCartImg from '../../images/empty_cart.png'
-import './LoginPage.css'
-
+import emptyCartImg from "../../images/empty_cart.png";
+import "./LoginPage.css";
 
 const loadCart = () => {
   const cart = localStorage.getItem("cart");
@@ -26,14 +25,12 @@ const loadCart = () => {
 };
 
 function LoginPage() {
-
   const [totalCart, setData] = useState(() => loadCart());
 
   /* Storing the cart in local storage. Every time that totalCart changes, we set that information into localStorage */
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(totalCart));
   }, [totalCart]);
-
 
   /**
    * If the id of the item in the array matches the id of the item that was clicked, then increment the
@@ -50,8 +47,6 @@ function LoginPage() {
     setData(newCart);
   };
 
-
-
   /**
    * If the item id matches the id passed in and the quantity is greater than 1, then decrement the
    * quantity by 1 when the user clicks on substract quantity button..
@@ -61,7 +56,7 @@ function LoginPage() {
     newCart.map((item) => {
       if (item.id === id) {
         item.quantity--;
-        
+
         //If the quantity is less than 1, remove the item from the cart
         if (item.quantity < 1) {
           item.quantity = 1;
@@ -73,8 +68,6 @@ function LoginPage() {
     });
     setData(newCart);
   };
-
-
 
   //Remove a product from the cart when the user clicks on the remove button and reset its quantity to 1
   const removeProduct = (id) => {
@@ -90,8 +83,6 @@ function LoginPage() {
     setData(newCart);
   };
 
-
-
   //Set the sum aff all products in the cart taking into account the quantity selected, and display it in the total price element of the cart
   const [totalPrice, setTotalPrice] = useState();
   useEffect(() => {
@@ -100,9 +91,6 @@ function LoginPage() {
     }, 0);
     setTotalPrice(total);
   }, [totalCart]);
-
-
-
 
   //Set the total quantity of products of the cart to display it in navbar icon
   const [totalQuantity, setTotalQuantity] = useState();
@@ -113,142 +101,237 @@ function LoginPage() {
     setTotalQuantity(total);
   }, [totalCart]);
 
-
-
-    /**
+  /**
    * CheckCart() is a function that returns a map of the totalCart array, which is an array of objects,
    * and maps each object to a ProductItem component, which is a component that renders a product item.
    * @returns The return statement is returning the result of the map function.
    */
-     const checkCart = () => {
-    
-      return totalCart.map((product) => (
-        <ProductItem
-          key={product.id}
-          id={product.id}
-          title={product.title}
-          price={product.price}
-          img={product.img}
-          addQuantity={addQuantity}
-          substractQuantity={substractQuantity}
-          quantity={product.quantity}
-          totalPrice={totalPrice}
-          removeProduct={removeProduct}
-        />
-      ));
-    };
-
-
+  const checkCart = () => {
+    return totalCart.map((product) => (
+      <ProductItem
+        key={product.id}
+        id={product.id}
+        title={product.title}
+        price={product.price}
+        img={product.img}
+        addQuantity={addQuantity}
+        substractQuantity={substractQuantity}
+        quantity={product.quantity}
+        totalPrice={totalPrice}
+        removeProduct={removeProduct}
+      />
+    ));
+  };
 
   const ListLength = totalCart.length;
   return (
     <>
-    <NavbarLogin totalQuantity = {totalQuantity} />
-    {ListLength === 0 ? (
-                <Cart title={"Your cart is empty."} totalPrice={0} emptyCartImg = {<img className='empty__cart' src={emptyCartImg} alt="Sad empty cart"/>} />
-              ) : (
-                <Cart
-                  title={""}
-                  totalPrice={totalPrice}
-                  productItem={checkCart()}
-                />
-              )}
+      <NavbarLogin totalQuantity={totalQuantity} />
+      {ListLength === 0 ? (
+        <Cart
+          title={"Your cart is empty."}
+          totalPrice={0}
+          emptyCartImg={
+            <img
+              className="empty__cart"
+              src={emptyCartImg}
+              alt="Sad empty cart"
+            />
+          }
+        />
+      ) : (
+        <Cart title={""} totalPrice={totalPrice} productItem={checkCart()} />
+      )}
 
-<ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
-        <Box
-          sx={{
-            marginTop: 8,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-          }}
-        >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign up
-          </Typography>
-          <Box component="form" noValidate sx={{ mt: 3 }}>
-            <Grid container spacing={2}>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  autoComplete="given-name"
-                  name="firstName"
-                  required
-                  fullWidth
-                  id="firstName"
-                  label="First Name"
-                  autoFocus
-                />
-              </Grid>
-              <Grid item xs={12} sm={6}>
-                <TextField
-                  required
-                  fullWidth
-                  id="lastName"
-                  label="Last Name"
-                  name="lastName"
-                  autoComplete="family-name"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  id="email"
-                  label="Email Address"
-                  name="email"
-                  autoComplete="email"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  required
-                  fullWidth
-                  name="password"
-                  label="Password"
-                  type="password"
-                  id="password"
-                  autoComplete="new-password"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <FormControlLabel
-                  control={<Checkbox value="allowExtraEmails" color="primary" />}
-                  label="I want to receive inspiration, marketing promotions and updates via email."
-                />
-              </Grid>
-            </Grid>
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign Up
-            </Button>
-            <Grid container justifyContent="flex-end">
-              <Grid item>
-                <Link href="#" variant="body2">
-                  Already have an account? Sign in
-                </Link>
-              </Grid>
-            </Grid>
-          </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+      <div className="login__wrapper">
+        <div className="login__signin">
+          <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+              <CssBaseline />
+              <Box
+                sx={{
+                  marginTop: 8,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Sign up
+                </Typography>
+                <Box component="form" noValidate sx={{ mt: 3 }}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        autoComplete="given-name"
+                        name="firstName"
+                        required
+                        fullWidth
+                        id="firstName"
+                        label="First Name"
+                        autoFocus
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        fullWidth
+                        id="lastName"
+                        label="Last Name"
+                        name="lastName"
+                        autoComplete="family-name"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <FormControlLabel
+                        control={
+                          <Checkbox value="allowExtraEmails" color="primary" />
+                        }
+                        label="I want to receive inspiration, marketing promotions and updates via email."
+                      />
+                    </Grid>
+                  </Grid>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Sign Up
+                  </Button>
+                  <Grid container justifyContent="flex-end">
+                    <Grid item>
+                      <Link href="#" variant="body2">
+                        Already have an account? Sign in
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Box>
+            </Container>
+          </ThemeProvider>
+        </div>
+
+        <div className="login__signup">
+          <ThemeProvider theme={theme}>
+            <Container component="main" maxWidth="xs">
+              <CssBaseline />
+              <Box
+                sx={{
+                  marginTop: 8,
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                }}
+              >
+                <Avatar sx={{ m: 1, bgcolor: "secondary.main" }}>
+                  <LockOutlinedIcon />
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                  Sign up
+                </Typography>
+                <Box component="form" noValidate sx={{ mt: 3 }}>
+                  <Grid container spacing={2}>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        autoComplete="given-name"
+                        name="firstName"
+                        required
+                        fullWidth
+                        id="firstName"
+                        label="First Name"
+                        autoFocus
+                      />
+                    </Grid>
+                    <Grid item xs={12} sm={6}>
+                      <TextField
+                        required
+                        fullWidth
+                        id="lastName"
+                        label="Last Name"
+                        name="lastName"
+                        autoComplete="family-name"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        id="email"
+                        label="Email Address"
+                        name="email"
+                        autoComplete="email"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <TextField
+                        required
+                        fullWidth
+                        name="password"
+                        label="Password"
+                        type="password"
+                        id="password"
+                        autoComplete="new-password"
+                      />
+                    </Grid>
+                    <Grid item xs={12}>
+                      <FormControlLabel
+                        control={
+                          <Checkbox value="allowExtraEmails" color="primary" />
+                        }
+                        label="I want to receive inspiration, marketing promotions and updates via email."
+                      />
+                    </Grid>
+                  </Grid>
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2 }}
+                  >
+                    Sign Up
+                  </Button>
+                  <Grid container justifyContent="flex-end">
+                    <Grid item>
+                      <Link href="#" variant="body2">
+                        Already have an account? Sign in
+                      </Link>
+                    </Grid>
+                  </Grid>
+                </Box>
+              </Box>
+            </Container>
+          </ThemeProvider>
+        </div>
+      </div>
     </>
   );
 }
 
 const theme = createTheme();
-
-
-
-
 
 export default LoginPage;
