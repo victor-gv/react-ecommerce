72abee818022e1029f01d95dc5fc3613996d862e
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-07-17 19:05:42 +0200

    signin - signup first structure created

--- src/components/LoginPage/LoginPage.js
+++ src/components/LoginPage/LoginPage.js
@@ -152,11 +152,11 @@ function LoginPage() {
                       <LockOutlinedIcon />
                     </Avatar>
                     <Typography component="h1" variant="h5">
-                      Sign up
+                      Sign in
                     </Typography>
                     <Box component="form" noValidate sx={{ mt: 3 }}>
                       <Grid container spacing={2}>
-                        <Grid item xs={12} sm={6}>
+                        {/* <Grid item xs={12} sm={6}>
                           <TextField
                             autoComplete="given-name"
                             name="firstName"
@@ -176,7 +176,7 @@ function LoginPage() {
                             name="lastName"
                             autoComplete="family-name"
                           />
-                        </Grid>
+                        </Grid> */}
                         <Grid item xs={12}>
                           <TextField
                             required
@@ -185,6 +185,7 @@ function LoginPage() {
                             label="Email Address"
                             name="email"
                             autoComplete="email"
+                            autoFocus
                           />
                         </Grid>
                         <Grid item xs={12}>
@@ -213,15 +214,8 @@ function LoginPage() {
                         variant="contained"
                         sx={{ mt: 3, mb: 2 }}
                       >
-                        Sign Up
+                        Sign In
                       </Button>
-                      <Grid container justifyContent="flex-end">
-                        <Grid item>
-                          <Link href="#" variant="body2">
-                            Already have an account? Sign in
-                          </Link>
-                        </Grid>
-                      </Grid>
                     </Box>
                   </Box>
                 </Container>
@@ -255,7 +249,6 @@ function LoginPage() {
                             fullWidth
                             id="firstName"
                             label="First Name"
-                            autoFocus
                           />
                         </Grid>
                         <Grid item xs={12} sm={6}>
@@ -306,13 +299,6 @@ function LoginPage() {
                       >
                         Sign Up
                       </Button>
-                      <Grid container justifyContent="flex-end">
-                        <Grid item>
-                          <Link href="#" variant="body2">
-                            Already have an account? Sign in
-                          </Link>
-                        </Grid>
-                      </Grid>
                     </Box>
                   </Box>
                 </Container>
