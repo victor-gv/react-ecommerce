7a92078177d9edf0b2bffdeb00655af38eac1673
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-07-19 22:36:07 +0200

    favs component created

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import MainPage from './components/MainPage/MainPage'
 import LoginPage from './components/LoginPage/LoginPage'
+import Favs from './components/Favs/Favs'
 import Footer from './components/Footer/Footer'
 import { Routes, Route } from 'react-router-dom'
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/' element={<><MainPage /><Footer /></>} />
         <Route path='/login' element={<><LoginPage /> <Footer /></>} />
+        <Route path='/favs' element={<><Favs /> <Footer /></>} />
       </Routes>
     </>
   )
