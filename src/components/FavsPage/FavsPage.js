7cf63b1701cb5e27f40b78d163f3568fe6d0fe60
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-07-23 23:23:37 +0200

    useSearch hook created

--- src/components/FavsPage/FavsPage.js
+++ src/components/FavsPage/FavsPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import useCounterCart from "../Hooks/useCounterCart";
+import useSearch from "../Hooks/useSearch";
 import Navbar from "../Navbar/Navbar";
 import { useEffect, useState, useReducer } from "react";
 import { Link } from "react-router-dom";
@@ -25,46 +26,7 @@ function FavsPage() {
     setTotalQuantity,
   } = useCounterCart();
 
-  //Search function
-  const searchItem = () => {
-    const search = document.getElementById("search").value;
-    const products = document.querySelectorAll(".card");
-
-    //Prevent refreshing the page when the user press enter key in the search bar
-    const form = document.getElementById("searchForm");
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-    });
-
-    /* A function that filters the products and renders only the ones that match the search. */
-    products.forEach((product) => {
-      //We take into account only the title of the product and the price of the product, but not the text of the buy button to match the search.
-      const title = product.querySelector(".card__title").innerText;
-      const price = product.querySelector(".card__shop__price").innerText;
-      const emptySearch = document.getElementById("emptySearch");
-
-      if (
-        title.toLowerCase().includes(search.toLowerCase()) ||
-        price.includes(search)
-      ) {
-        product.style.display = "block";
-        product.setAttribute("matched", "true");
-      } else {
-        product.style.display = "none";
-        product.setAttribute("matched", "false");
-      }
-
-      const unMatchedProducts = document.querySelectorAll("[matched='false']");
-      //If all the products have the attribute matched set to false, we toggle the display of the emptySearch div to display the message "No results found".
-      if (unMatchedProducts.length === products.length) {
-        emptySearch.classList.add("empty-alert");
-        emptySearch.classList.remove("hidden");
-      } else {
-        emptySearch.classList.add("hidden");
-        emptySearch.classList.remove("empty-alert");
-      }
-    });
-  };
+  const { searchItem } = useSearch();
 
   //Obtain the product data from the Products component and pass it to the Cart component
   const addToCart = (product) => {
--- src/components/MainPage/MainPage.js
+++ src/components/MainPage/MainPage.js
@@ -9,52 +9,13 @@ import emptyCartImg from "../../images/empty_cart.png";
 import noResult from "../../images/no-results.png";
 import favsReducer from "../FavsReducer/FavsReducer";
 import useCounterCart from "../Hooks/useCounterCart";
+import useSearch from "../Hooks/useSearch";
 import "./MainPage.css";
 
 function MainPage() {
 
   const { addQuantity, substractQuantity, removeProduct, totalPrice, totalCart, setData, totalQuantity, setTotalQuantity} = useCounterCart();
-
-  //Search function
-  const searchItem = () => {
-    const search = document.getElementById("search").value;
-    const products = document.querySelectorAll(".card");
-
-    //Prevent refreshing the page when the user press enter key in the search bar
-    const form = document.getElementById("searchForm");
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-    });
-
-    /* A function that filters the products and renders only the ones that match the search. */
-    products.forEach((product) => {
-      //We take into account only the title of the product and the price of the product, but not the text of the buy button to match the search.
-      const title = product.querySelector(".card__title").innerText;
-      const price = product.querySelector(".card__shop__price").innerText;
-      const emptySearch = document.getElementById("emptySearch");
-
-      if (
-        title.toLowerCase().includes(search.toLowerCase()) ||
-        price.includes(search)
-      ) {
-        product.style.display = "block";
-        product.setAttribute("matched", "true");
-      } else {
-        product.style.display = "none";
-        product.setAttribute("matched", "false");
-      }
-
-      const unMatchedProducts = document.querySelectorAll("[matched='false']");
-      //If all the products have the attribute matched set to false, we toggle the display of the emptySearch div to display the message "No results found".
-      if (unMatchedProducts.length === products.length) {
-        emptySearch.classList.add("empty-alert");
-        emptySearch.classList.remove("hidden");
-      } else {
-        emptySearch.classList.add("hidden");
-        emptySearch.classList.remove("empty-alert");
-      }
-    });
-  };
+  const { searchItem } = useSearch();
 
 
 
