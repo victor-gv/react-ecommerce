4eca4c86bf476d1b60526a37d47aef6fa2992e49
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-07-29 13:28:41 +0200

    improved productRelated by not showing the same product that is being displayed as main

--- src/components/ProductPage/ProductRelated/ProductRelated.js
+++ src/components/ProductPage/ProductRelated/ProductRelated.js
@@ -1,19 +1,26 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import useFetch from "../../Hooks/useFetch";
 
 const ProductRelated = () => {
 
   const { products } = useFetch();
 
+  const location = useLocation();
+  const mainProduct = location.state;
+
   return (
     <>
-      {products.length > 0 &&
+      {
         products
+          /* Filtering out the product that is currently being viewed. */
+          .filter(product => product.id !== mainProduct.id)
+          /* Sorting the array randomly. */
           .sort(() => Math.random() - Math.random())
+          /* Limiting the number of products to 4. */
           .slice(0, 4)
           .map((product) => (
-            <div key={product.id} className="col-4">
+              <div key={product.id} className="col-4">
               <Link to={`/product/${product.id}`} state={product}>
                 <img src={product.img} alt={product.title} />
                 <h4>{product.title}</h4>
@@ -21,6 +28,7 @@ const ProductRelated = () => {
                 <h5>{product.price}€</h5>
               </Link>
             </div>
+
           ))}
     </>
   );
