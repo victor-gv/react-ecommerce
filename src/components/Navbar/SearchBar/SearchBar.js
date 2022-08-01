cc069a331e84898c01aabbbda3470bfc1bf946a2
Victor Garcia-Vao <[victorgarciavao@gmail.com]>
2022-08-01 18:27:40 +0200

    added value to searchBar

--- src/components/Navbar/SearchBar/SearchBar.js
+++ src/components/Navbar/SearchBar/SearchBar.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import { FormControl } from 'react-bootstrap'
+import useSearch from '../../Hooks/useSearch'
 
 const SearchBar = (props) => {
+  const { filter } = useSearch();
   return (
     <div className="search-form">
       <FormControl
         type="search"
         placeholder="Search"
         className="me-2"
+        value={filter}
         aria-label="Search"
         onChange={props.manageChange}
       />
